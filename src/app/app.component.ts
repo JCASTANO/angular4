5322a2eb72284665b452f5413e94eb15d32a4109
Juan Esteban <juan.castano@ceiba.com.co>
2018-08-19 11:20:15 -0500

    GRAL:Http loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { AutorizacionService } from './services/autorizacion.service';
 import { Component } from '@angular/core';
-import { Spinkit } from 'ng-http-loader';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +7,6 @@ import { Spinkit } from 'ng-http-loader';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public spinkit = Spinkit;
   public loggedIn = false;
   public email = null;
 
